4886d4e72f41d0907a0ac18a4839ffbd88ddc37f
Stijn de Witt <StijnDeWitt@hotmail.com>
2016-11-07 13:57:19 +0100

    1.4.0: More logging, less code  * Use extend i.s.o hand-rolled code    (this will most likely end up saving space in the browser build)  * Logging in every file  * Fixed issue with log is undefined

--- bin/i18nline.js
+++ bin/i18nline.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 
-var minimist = require('minimist');
-var argv = minimist(process.argv.slice(2));
 var log = require('../lib/log')('i18nline', argv.s || argv.silent)
+
+var minimist = require('minimist');
+var extend = require('extend');
+var argv = minimist(process.argv.slice(2));
 var I18nline = require('../lib/main');
-var Utils = require('../lib/utils');
 
 var config = I18nline.config;
 var command = argv._[0];
-var options = Utils.extend({}, config, argv);
+var options = extend({}, config, argv);
 options.patterns = typeof options.patterns == 'string' ? options.patterns.split(',') : options.patterns || [];
 options.ignorePatterns = typeof options.ignorePatterns == 'string' ? options.ignorePatterns.split(',') : options.ignorePatterns || [];
 options.directories = typeof options.directories == 'string' ? options.directories.split(',') : options.directories;
