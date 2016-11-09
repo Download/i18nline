26f6bb8f18c58a57c02b4e09bd71198a1ae485a4
Stijn de Witt <StijnDeWitt@hotmail.com>
2016-11-09 09:53:11 +0100

    1.4.1: Bugfix release  * Make sure configuration options are always normalized correctly  * Decreased average log level

--- bin/i18nline.js
+++ bin/i18nline.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
 
-var log = require('../lib/log')('i18nline', argv.s || argv.silent)
-
 var minimist = require('minimist');
 var extend = require('extend');
 var argv = minimist(process.argv.slice(2));
+
+var log = require('../lib/log')('i18nline', argv.s || argv.silent)
+
 var I18nline = require('../lib/main');
+var normalize = require('../lib/load-config').normalize;
 
 var config = I18nline.config;
 var command = argv._[0];
-var options = extend({}, config, argv);
-options.patterns = typeof options.patterns == 'string' ? options.patterns.split(',') : options.patterns || [];
-options.ignorePatterns = typeof options.ignorePatterns == 'string' ? options.ignorePatterns.split(',') : options.ignorePatterns || [];
-options.directories = typeof options.directories == 'string' ? options.directories.split(',') : options.directories;
+var options = normalize(extend({}, config, argv));
 
 I18nline.Commands.run(command, options);
