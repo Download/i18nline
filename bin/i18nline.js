790862a694cf430f8c4159f6f2aab931b26c2178
Stijn de Witt <StijnDeWitt@hotmail.com>
2016-11-09 10:52:47 +0100

    1.4.2: Bugfix release  * Fixed bug in configuration handling  * Finally configuration is uniform over package.json, .i18nrc and CLI  * Can specify options that accept array and string as sting in all cases:    * plugins    * patterns    * ignorePatterns    * directories    * ignoreDirectories

--- bin/i18nline.js
+++ bin/i18nline.js
@@ -6,11 +6,7 @@ var argv = minimist(process.argv.slice(2));
 
 var log = require('../lib/log')('i18nline', argv.s || argv.silent)
 
-var I18nline = require('../lib/main');
-var normalize = require('../lib/load-config').normalize;
-
-var config = I18nline.config;
 var command = argv._[0];
-var options = normalize(extend({}, config, argv));
-
-I18nline.Commands.run(command, options);
+delete argv._
+var I18nline = require('../lib/main').configure(argv);
+I18nline.Commands.run(command, I18nline.config);
